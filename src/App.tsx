45ddaaa840a1cc7e9798ef5d96051a803511ca96
pakiyalakshmi <134521823+pakiyalakshmi@users.noreply.github.com>
2025-01-22 19:41:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,5 +21,7 @@ const App = () => (
     </TooltipProvider>
   </QueryClientProvider>
 );
+
+
 
 export default App;
